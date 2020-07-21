7f0a4b26176c80e6c578bd10ae749586d789ffd9
vy-hoang <57076819+vy-hoang@users.noreply.github.com>
2020-07-21 17:17:49 -0400

    Updates

---
+++ precache-manifest.3b91297c78cc60b9baef98d06a4857ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d2bfe97f8319a29811ca17697bdadc",
+    "url": "/Reminder-app-/index.html"
+  },
+  {
+    "revision": "647cc7563b30a9df6673",
+    "url": "/Reminder-app-/static/js/2.99d717be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reminder-app-/static/js/2.99d717be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f104ce2f85ea0e49707a",
+    "url": "/Reminder-app-/static/js/main.740d5c71.chunk.js"
+  },
+  {
+    "revision": "02a4630ba03dbbbb0c38",
+    "url": "/Reminder-app-/static/js/runtime-main.0ddc68dc.js"
+  }
+]);
